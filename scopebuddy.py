41332f4d8a3ed518d5c56e222c093f612a6bb71e
aa <aaa>
2023-11-09 10:16:19 +0800

    Added RDNS caching, added local BGP database use, made WHOIS option, it is now *a lot* faster

--- scopebuddy.py
+++ scopebuddy.py
@@ -1,37 +1,45 @@
 from concurrent.futures import ThreadPoolExecutor
-import threading
 from contextlib import contextmanager
-import argparse
-import time
-import socket
 from ipwhois import IPWhois
-import ipaddress
 from pprint import pprint
-import warnings
-import shodan
-import sys
+import argparse
+import csv
+import dns.resolver
+import ipaddress
 import json
 import os
-import csv
+import pyasn
+import requests
+import shodan
+import socket
+import subprocess
+import sys
+import threading
+import time
+import warnings
+
 
 warnings.filterwarnings("ignore")
 
 cache = {}
 shodan_cache = {}
+cymru_asn_cache = {}
+rdns_cache = {}
 last_ipwhois_timestamp = 0  # Initialize the timestamp
 lock = threading.Lock()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dnslist", help="A text file containing a list of domain names")
 parser.add_argument("-s", "--shodan", default=True, action="store_false", help="Disable Shodan search against discovered IP addresses")
+parser.add_argument("-w", "--whois", default=False, action="store_true", help="Enable WHOIS functionality for IP ownership, this will slow things down dramatically")
 parser.add_argument("-c", "--config", default=f"{os.path.dirname(os.path.realpath(__file__))}/config.json", help="Provide a config file containing API keys for additional services (e.g. Shodan)")
 parser.add_argument("-o", "--output", default="-", help="Output file")
 parser.add_argument("-v", "--verbose", default=0, action="count", help="Increase verbosity level (use -v for normal verbosity, -vv for more verbosity)")
 args = parser.parse_args()
 shodan_enable = args.shodan
+whois_enable = args.whois
 verbose = args.verbose
 output = args.output
-
 
 
 if shodan_enable:
@@ -65,6 +73,82 @@ def print_debug(output, level):
     if verbose >= level:
         sys.stderr.write(f"{output}\n")
         sys.stderr.flush()
+
+
+# Function to download and convert BGP data using pyasn utilities
+def download_and_convert_bgp_data(cache_dir):
+    db_path = os.path.join(cache_dir, "ipasn.json")
+    dump_path = os.path.join(cache_dir, "latest.bz2")
+    file_age = time.time() - os.path.getmtime(db_path) if os.path.exists(db_path) else None
+
+    if not file_age or file_age >= 6 * 3600:
+        # Download the BGP data dump if it doesn't exist or is older than 6 hours
+        print(f"[*] Downloading BGP data dump as cached copy is not present or is older than 6 hours")
+        download_command = f"pyasn_util_download.py --latestv46 --filename {dump_path}"
+        print_debug(f'[*] Running {download_command}', 2)
+        if verbose == False:
+            subprocess.run(download_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            subprocess.run(download_command, shell=True)
+        print(f"[*] Download of BGP data dump complete")
+
+
+        # Convert the BGP data to the pyasn format
+        print(f"[*] Converting BGP data dump to pyasn format")
+        convert_command = f"pyasn_util_convert.py --single {dump_path} {db_path}"
+        print_debug(f'[*] Running {convert_command}', 2)
+        if verbose == False:
+            subprocess.run(convert_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            subprocess.run(convert_command, shell=True)
+        print(f"[*] Conversion of BGP data dump complete")
+
+
+    return pyasn.pyasn(db_path)
+
+def get_asn_bgpdb(ip, asndb):
+    asn, _ = asndb.lookup(ip)
+    return asn
+
+def get_cidr_bgpdb(ip, asndb):
+    _, cidr = asndb.lookup(ip)
+    return cidr
+
+
+def search_cymru_cache(asn_number):
+
+    if asn_number in cymru_asn_cache.keys():
+        print_debug(f"[*] Cache hit for ASN: {asn_number}", 2)
+        return cymru_asn_cache[asn_number]
+    else:
+        print_debug(f"[*] Cache miss for ASN: {asn_number}", 2)
+        return False
+
+def get_asn_name_cymru(ip, asndb):
+
+
+    asn_number = get_asn_bgpdb(ip, asndb)
+    result = search_cymru_cache(asn_number)
+
+    if result:
+        return result
+
+    try:
+        asn_query = f"AS{asn_number}.asn.cymru.com"
+        answers = dns.resolver.resolve(asn_query, "TXT")
+        for rdata in answers:
+            for txt_string in rdata.strings:
+                txt_string = txt_string.decode("utf-8")  # Decode bytes to string
+                if txt_string.startswith(f"{asn_number} |"):
+                    asn_name = txt_string.split("|")[-1].strip()
+                    cymru_asn_cache[asn_number] = asn_name
+                    return asn_name
+    
+    except Exception as e:
+        return "No Data/Failed"
+
+    return None
+
 
 def search_cache(ip):
     for item in cache.keys():
@@ -106,11 +190,29 @@ def get_ip(d):
         print_debug(f'[-] Could not resolve domain {d}', 1)
         return False
 
+
+def search_rnds_cache(ip):
+
+    if ip in rdns_cache.keys():
+        print_debug(f"[*] Cache hit for RDNS: {ip}", 2)
+        return rdns_cache[ip]
+    else:
+        print_debug(f"[*] Cache miss for RDNS: {ip}", 2)
+        return False
+
+
 def get_rdns(ip):
+
+    result = search_rnds_cache(ip)
+
+    if result:
+        return result
+
     try:
         data = socket.gethostbyaddr(ip)
         host = data[0]
         print_debug(f'[+] IP: {ip}, RDNS {host}', 1)
+        rdns_cache[ip] = host
         return host
     except Exception:
         print_debug(f'[-] No RDNS found for {ip}', 1)
@@ -179,7 +281,7 @@ def get_shodan_ports(host):
     except:
         return "No Data/Failed"
 
-def process_domain(domain):
+def process_domain(domain, asndb, whois_enabled):
     data = get_ip(domain)
     if data:
         results = []
@@ -188,12 +290,13 @@ def process_domain(domain):
                 "IP": ip,
                 "DNS": domain,
                 "RDNS": get_rdns(ip),
-                "ASN": get_asn(ip),
-                "IP Hoster": get_ip_hoster(ip),
-                "IP Owner": get_ip_owner(ip),
-                "BGP CIDR": get_bgp_cidr(ip),
-                "Whois CIDR": get_whois_cidr(ip)
+                "ASN": get_asn_bgpdb(ip, asndb),
+                "IP Hoster": get_asn_name_cymru(ip, asndb),
+                "BGP CIDR": get_cidr_bgpdb(ip, asndb)
             }
+            if whois_enabled:
+                result["IP Owner"] = get_ip_owner(ip)
+                result["Whois CIDR"] = get_whois_cidr(ip)
             if shodan_enable:
                 host = shodan_search(ip)
                 result["Shodan Ports"] = get_shodan_ports(host)
@@ -203,26 +306,45 @@ def process_domain(domain):
         return []  # Return an empty list when no IP address is associated with the domain
 
 
+
 def main():
+
+    cache_dir = os.path.expanduser("~/.cache/scopebuddy/ipasn")
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+
+    # Download and convert BGP data
+    asndb = download_and_convert_bgp_data(cache_dir)
+
     domains = [line.rstrip('\n') for line in open(args.dnslist)]
     
     with open_or_stdout(output) as f:
         writer = csv.writer(f)
         
+        header = ["IP", "DNS", "RDNS", "ASN", "IP Hoster", "BGP CIDR"]
+        if whois_enable:
+            header.append("IP Owner")
+            header.append("Whois CIDR")
         if shodan_enable:
-            writer.writerow(["IP", "DNS", "RDNS", "ASN", "IP Hoster", "IP Owner", "BGP CIDR", "Whois CIDR", "Shodan Ports"])
-        else:
-            writer.writerow(["IP", "DNS", "RDNS", "ASN", "IP Hoster", "IP Owner", "BGP CIDR", "Whois CIDR"])
+            header.append("Shodan Ports")
         
-        with ThreadPoolExecutor(max_workers=10) as executor:
+        writer.writerow(header)
+        
+        with ThreadPoolExecutor(max_workers=20) as executor:
             for domain in domains:
                 domain = domain.strip()
-                results = executor.submit(process_domain, domain)
+                results = executor.submit(process_domain, domain, asndb, whois_enable)
                 for result in results.result():
-                    row = [result["IP"], result["DNS"], result["RDNS"], result["ASN"], result["IP Hoster"], result["IP Owner"], result["BGP CIDR"], result["Whois CIDR"]]
+                    row = [result["IP"], result["DNS"], result["RDNS"], result["ASN"], result["IP Hoster"], result["BGP CIDR"]]
+                    if whois_enable:
+                        row.append(result["IP Owner"])
+                        row.append(result["Whois CIDR"])
                     if shodan_enable:
                         row.append(result.get("Shodan Ports", "No Data/Failed"))
                     writer.writerow(row)
+
 
 if __name__ == "__main__":
     main()
